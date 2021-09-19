534fd4066d5bc05c7fde43b76369fee59f152348
Scott Reilly <scott@coffee2code.com>
2021-09-19 00:00:02 -0700

    Unit tests: In bootstrap, store path to plugin file constant

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -5,6 +5,8 @@
  * @package Random_File
  */
 
+define( 'RANDOM_FILE_PLUGIN_FILE', dirname( __FILE__, 3 ) . '/random-file.php' );
+
 ini_set( 'display_errors', 'on' );
 error_reporting( E_ALL );
 
@@ -20,7 +22,7 @@ require_once $_tests_dir . '/tests/phpunit/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( __FILE__, 3 ) . '/random-file.php';
+	require RANDOM_FILE_PLUGIN_FILE;
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
